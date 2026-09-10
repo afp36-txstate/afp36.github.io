4040f0d9da3df20e6bedacc7562b3133edf3b7c3
afp36-txstate <afp36@txstate.edu>
2026-09-09 19:51:16 -0500

    M

--- JavaScript/external.js
+++ JavaScript/external.js
@@ -2,7 +2,7 @@
 // window.alert(5+6)
 // window.alert('5 + 6');
 // document.write('<h1>A heading</h1>');
-const x = "web";
-const y = "mapping";
-const out = x + y;
-document.write(out);
+ const x = "web";
+ const y = "mapping";
+ const out = x + y;
+ document.write(out);
