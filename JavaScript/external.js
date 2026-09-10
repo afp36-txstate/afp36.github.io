09f3b807d0841e3ef36acd15ffbbef55624c9052
afp36-txstate <afp36@txstate.edu>
2026-09-09 20:03:27 -0500

    fixed

--- JavaScript/external.js
+++ JavaScript/external.js
@@ -1,8 +1,1 @@
-// (document.write("Where is my JS code?");
-// window.alert(5+6)
-// window.alert('5 + 6');
-// document.write('<h1>A heading</h1>');
- const x = "web";
- const y = "mapping";
- const out = x + y;
- document.write(out);
+ document.write("Where is my JS code?");
