59e6459bdfd6a3c6f95cb2eff24e7bbee7aedd26
afp36-txstate <afp36@txstate.edu>
2026-09-16 14:04:00 -0500

    task 3 completed

--- JavaScript/external.js
+++ JavaScript/external.js
@@ -7,3 +7,6 @@ const x = "web";
 const y = "mapping";
 const out = x + y;
 document.write(out);
+// A prompt box is used to prompt users to input a value before entering a page.
+user_name = window.prompt(" Andrew Pearson", "Andrew Pearson");
+document.write(user_name);
