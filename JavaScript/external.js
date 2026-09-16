2284236e15e3ccb583590daf55029ea7af029364
afp36-txstate <afp36@txstate.edu>
2026-09-16 14:00:54 -0500

    out

--- JavaScript/external.js
+++ JavaScript/external.js
@@ -2,4 +2,8 @@
 // window.alert(5 + 6);
 // window.alert('5 + 6');
 // document.write('<h1>A heading</h1>');
-document.write('<p>A sentence.</p>')
+// document.write('<p>A sentence.</p>')
+const x = "web";
+const y = "mapping";
+const out = x + y;
+document.write(out);
