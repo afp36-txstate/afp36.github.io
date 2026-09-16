4117c5fd01f08d2d611e22f3aa9173c659e134b6
afp36-txstate <afp36@txstate.edu>
2026-09-16 14:11:03 -0500

    Task 4 complete

--- JavaScript/external.js
+++ JavaScript/external.js
@@ -3,15 +3,17 @@
 // window.alert('5 + 6');
 // document.write('<h1>A heading</h1>');
 // document.write('<p>A sentence.</p>')
-const x = "web";
-const y = "mapping";
-const out = x + y;
-document.write(out);
-// A prompt box is used to prompt users to input a value before entering a page.
-<<<<<<< HEAD
-afp36 = window.prompt(" Andrew Pearson", "Andrew Pearson");
-document.write(afp36);
-=======
-user_name = window.prompt(" Andrew Pearson", "Andrew Pearson");
-document.write(user_name);
->>>>>>> 59e6459bdfd6a3c6f95cb2eff24e7bbee7aedd26
+// ca = 22;
+// const x = "web";
+// const y = "mapping";
+// const out = x + y;
+// document.write(out);
+// // A prompt box is used to prompt users to input a value before entering a page.
+// afp36 = window.prompt("Andrew Pearosn", "Andrew Pearson");
+// document.write(afp36);
+const b = 33;
+document.writeln(a + b);
+document.writeln("<br>");
+document.writeln(a += b);
+document.writeln("<br>");
+document.writeln(a += b);
