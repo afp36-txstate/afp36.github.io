53fe69d842933118e3dad2f1049e58df01f47738
afp36-txstate <afp36@txstate.edu>
2026-09-23 15:08:47 -0500

    Filled content from webmaps array into the webmaps table

--- JavaScript/external.js
+++ JavaScript/external.js
@@ -3,24 +3,14 @@
 // {
 // document.writeln(arr[i],"<br>");
 // }
+
 var webmaps =
 [
   ["Oil Spill Toolkit", "https://www.glo.texas.gov", "The oil spill toolkit
 developed by Enterprise Technology Solutions is neat."],
   ["Texas Ecosystems Analytical Mapper", "http://tpwd.texas.gov/gis/team/", "The
 Texas Parks and Wildlife's Landscape Ecology program is great."]
-];
-
-function welcome()
-{
-    let a = "Please enter your name.";
-    let b = "Type your name here.";
-// A prompt box is used to prompt users to input a value before entering a page.
-    user_name = window.prompt(a, b);
-    message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
-    return message
-}
-
+]
 function webmap_table()
 {
   document.write("<table width=100%>");
@@ -35,4 +25,13 @@ function webmap_table()
  }
  document.write("</table>");
  return "";
+}
+function welcome()
+{
+    let a = "Please enter your name.";
+    let b = "Type your name here.";
+// A prompt box is used to prompt users to input a value before entering a page.
+    user_name = window.prompt(a, b);
+    message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+    return message
 }
