0e0442216af02180b726e8fcddbf64183a8921c3
afp36-txstate <afp36@txstate.edu>
2026-09-23 14:53:56 -0500

    Filled content from webmaps array into the webmaps table

--- JavaScript/external.js
+++ JavaScript/external.js
@@ -3,6 +3,13 @@
 // {
 // document.writeln(arr[i],"<br>");
 // }
+var webmaps =
+[
+  ["Oil Spill Toolkit", "https://www.glo.texas.gov", "The oil spill toolkit
+developed by Enterprise Technology Solutions is neat."],
+  ["Texas Ecosystems Analytical Mapper", "http://tpwd.texas.gov/gis/team/", "The
+Texas Parks and Wildlife's Landscape Ecology program is great."]
+];
 function welcome()
 {
     let a = "Please enter your name.";
@@ -15,15 +22,15 @@ function welcome()
 function webmap_table()
 {
   document.write("<table width=100%>");
- for (var row=0; row < 2; row++)
+ for (var row=0; row < webmaps.length; row++)
  {
    document.write("<tr>");
-   for (var column=0; column < 3; column++)
+   for (var column=0; column < webmaps[0].length; column++)
    {
-     document.write("<td>" + row + "," + column + "</td>");
+     document.write("<td>" + webmaps[row][column] + "</td>");
    }
    document.write("</tr>");
  }
  document.write("</table>");
  return "";
-} 
+}
